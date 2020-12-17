2a13cb4dafcafd54fffa4083d5909c89d74628ff
percist <percist@gmail.com>
2020-12-17 11:05:33 -0500

    login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,21 +1,36 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// const { User } = require('../../db/models');
+// const { requireAuth } = require('../../utils/auth.js');
 
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      },
-    })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
+router.use('/session', sessionRouter);
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
+router.use('/users', usersRouter);
+
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.post('/test', (req, res) => {
+//     res.json({ requestBody: req.body });
+// });
 
 
 
