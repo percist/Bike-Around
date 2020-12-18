ee0d1ff558fb76d6e51df793839a6e6c8bf8b933
percist <percist@gmail.com>
2020-12-17 20:55:11 -0500

    Navbar complete with limited styling

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import "./Navigation.css"
 
-const ProfileButton = ({ user }) {
-    
+const ProfileButton = ({ user }) => {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
     return (
-        <div>
-        <i class="fas fa-user"></i>
-        </div>
+        <>
+            <button 
+                className="button" 
+                onClick={openMenu}
+            >
+                <i className="fas fa-user"></i>                
+            </button>
+            {showMenu && (
+                <ul className="profile-dropdown">
+                    <li>{user.username}</li>
+                    <li>{user.email}</li>
+                    <li>
+                        <button 
+                            className="button" 
+                            onClick={logout}
+                        >Log Out
+                        </button>
+                    </li>
+                </ul>
+            )}
+        </>
     );
 };
 
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 const Navigation = ({ isLoaded }) =>{
@@ -14,16 +15,22 @@ const Navigation = ({ isLoaded }) =>{
     }else {
         sessionLinks = (
             <>
-                <NavLink to="/login">Log In</NavLink>
-                <NavLink to="/signup">Sign Up</NavLink>
+                <a className="navlink">
+                    <NavLink to="/login">Log In</NavLink>                    
+                </a>
+                <a className="navlink">
+                    <NavLink to="/signup">Sign Up</NavLink>
+                </a>
             </>
         );
     }
 
     return (
-        <ul>
+        <ul className="navbar">
             <li>
-                <NavLink exact to="/">Home</NavLink>
+                <a className="navlink">
+                    <NavLink exact to="/">Home</NavLink>
+                </a>
                 {isLoaded && sessionLinks}
             </li>
         </ul>
