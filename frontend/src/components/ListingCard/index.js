5f3f9366fbaa8ee3d60e3faae8a528e601b19cbc
percist <percist@gmail.com>
2021-03-06 14:14:51 -0500

    styling on listing cards -final?

--- frontend/src/components/ListingCard/index.js
+++ frontend/src/components/ListingCard/index.js
@@ -1,33 +1,31 @@
 import './ListingCard.css';
 
 const ListingCard = ({ theListing }) => {
-    return (
-        <>
-            <div className="listing-card">
-                <div className="listing-card-container">
-                    <a href={`/listings/${theListing.id}`}>
-                        <img 
-                            alt="bike" 
-                            className="listing-card-img"
-                            src={theListing.Pictures[0].url} 
-                        />
-                    </a>
-                </div>
-                <div className="listing-content">
-                    <div className='listing-card-content_2'>
-                        {`${theListing.BikeType.type} bike - ${theListing.nearestCity}`}
-                    </div>
-                    <div className='listing-card-content_3'>
-                        {theListing.title}
-                    </div>
-                    <div className='listing-card-content_4'>
-                        {`$${theListing.pricePerDay/100} / day`}
-                    </div>
-                    <hr className='listing-card-bar' color="#896a67" />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className="listing-card">
+      <div className="listing-card-container">
+        <a href={`/listings/${theListing.id}`}>
+          <img
+            alt="bike"
+            className="listing-card-img"
+            src={theListing.Pictures[0].url}
+          />
+        </a>
+      </div>
+      <div className="listing-content">
+        <div className='listing-card-content_2'>
+          {`${theListing.BikeType.type} bike - ${theListing.nearestCity}`}
+        </div>
+        <div className='listing-card-content_3'>
+          {theListing.title}
+        </div>
+        <div className='listing-card-content_4'>
+          {`$${theListing.pricePerDay / 100} / day`}
+        </div>
+        <hr className='listing-card-bar' color="#896a67" />
+      </div>
+    </div>
+  )
 }
 
 export default ListingCard;
