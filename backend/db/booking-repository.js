179ee45b50fbc6554a9d35b4fad62ad7c0840fe8
percist <percist@gmail.com>
2021-03-06 19:20:19 -0500

    updating meta tags

--- backend/db/booking-repository.js
+++ backend/db/booking-repository.js
@@ -1,65 +1,76 @@
 const { sequelize, Booking, Listing } = require("./models");
 
 async function list() {
-    return await Booking.findAll();
+  return await Booking.findAll();
 }
 
 async function userList(id) {
-    const bookings = await Booking.findAll({
-        where: {
-            userId: id
-        }, 
-    })
-    const listing = await Listing.findOne({
-        id: bookings[0].dataValues.listingId,
-        include: {
-            all: true
-        }
+  const bookings = await Booking.findAll({
+    where: {
+      userId: id
+    },
+  })
+  const listing = await Listing.findOne({
+    id: bookings[0].dataValues.listingId,
+    include: {
+      all: true
     }
-        
-        )
-    return { bookings, listing }
+  }
+
+  )
+  return { bookings, listing }
 }
 
 async function getOne(id) {
-    const booking = await Booking.findByPk(id);
-    return booking;
+  const booking = await Booking.findByPk(id);
+  return booking;
 }
 
 async function create(details) {
-    //need logic here to check and see if a booking already exists with the same dates
+  const allListingBookings = await Booking.findAll({
+    where: {
+      listingId: details.listingId
+    }
+  })
+  const booked = allListingBookings.findIndex(booking => 
+    !(booking.startDay > new Date(details.endDay) || booking.endDay < new Date(details.startDay)) 
+    )
+  if (booked === -1){
     const booking = await Booking.create(details)
     return booking;
+  } else {
+    return null
+  }
 }
 
 async function confirm(id) {
-    const booking = await Booking.findByPk(id)
-    await booking.update({status: 'confirmed'})
-    await sequelize.close;
-    return booking;
+  const booking = await Booking.findByPk(id)
+  await booking.update({ status: 'confirmed' })
+  await sequelize.close;
+  return booking;
 };
 
-async function edit(details) {  
-    const booking = await Booking.findByPk(details.bookingId)  
-    const updatedBooking = await booking.update(
-        details,
-        );
-    return booking;
+async function edit(details) {
+  const booking = await Booking.findByPk(details.bookingId)
+  const updatedBooking = await booking.update(
+    details,
+  );
+  return booking;
 }
 
 async function cancelBooking(id) {
-    const booking = await Booking.findByPk(id)
-    await booking.update({status: 'cancelled'})
-    await sequelize.close;
-    return booking;
+  const booking = await Booking.findByPk(id)
+  await booking.update({ status: 'cancelled' })
+  await sequelize.close;
+  return booking;
 }
 
 module.exports = {
-    list,
-    userList,
-    getOne,
-    create,
-    confirm,
-    edit,
-    cancelBooking
+  list,
+  userList,
+  getOne,
+  create,
+  confirm,
+  edit,
+  cancelBooking
 }
--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,54 +9,58 @@ const router = express.Router();
 
 // retrieve all bookings for a single user
 router.get(
-  '/', 
-restoreUser, 
-asyncHandler(async (req, res, next) => {
+  '/',
+  restoreUser,
+  asyncHandler(async (req, res, next) => {
     const user = await req.user.toJSON()
     const { bookings, listing } = await BookingRepository.userList(user.id);
     res.json({ bookings, listing })
-}));
+  }));
 
 // retrieve a single booking
 router.get(
-  '/:id(\\d+)', 
-restoreUser, 
-asyncHandler(async (req, res, next) => {
+  '/:id(\\d+)',
+  restoreUser,
+  asyncHandler(async (req, res, next) => {
     const booking = await BookingRepository.getOne(req.params.id)
     res.json(booking);
-}));
+  }));
 
 // create a single booking
 router.post(
-  '/', 
-asyncHandler(async (req, res, next) => {
+  '/',
+  asyncHandler(async (req, res, next) => {
     const booking = await BookingRepository.create(req.body);
-    res.json(booking);
-}));
+    if (booking) {
+      res.json(booking);
+    }else{
+      res.json(false)
+    }
+  }));
 
 // edit a booking
 router.put(
-  '/:id(\\d+)', 
-asyncHandler(async (req, res, next) => {
+  '/:id(\\d+)',
+  asyncHandler(async (req, res, next) => {
     const booking = await BookingRepository.edit(req.body)
     res.json(booking);
-}));
+  }));
 
 // confirm a booking
 router.put(
-  '/:id(\\d+)/confirm', 
-asyncHandler(async (req, res, next) => {
+  '/:id(\\d+)/confirm',
+  asyncHandler(async (req, res, next) => {
     const booking = await BookingRepository.confirm(req.params.id)
     res.json(booking);
-}));
+  }));
 
 // delete a booking
 router.patch(
-  '/:id(\\d+)', 
-asyncHandler(async (req, res, next) => {
+  '/:id(\\d+)',
+  asyncHandler(async (req, res, next) => {
     const booking = await BookingRepository.cancelBooking(req.params.id)
     res.json(booking);
-}));
+  }));
 
 
 module.exports = router;
