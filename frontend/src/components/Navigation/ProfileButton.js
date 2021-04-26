565262e2fe9661862bdd3c7296c378fcb63cdf2c
percist <percist@gmail.com>
2021-04-25 21:00:39 -0400

    media query styling for navbar

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,58 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
 import "./Navigation.css"
 
 const ProfileButton = ({ user }) => {
-    const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
-    const openMenu = () => {
-        if (showMenu) return;
-        setShowMenu(true);
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
     };
 
-    useEffect(() => {
-        if (!showMenu) return;
+    document.addEventListener('click', closeMenu);
 
-        const closeMenu = () => {
-            setShowMenu(false);
-        };
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-        document.addEventListener('click', closeMenu);
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
 
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
+  return (
+    <>
+      <div
+        id="link-profile"
+        className="link"
+        onClick={openMenu}
+      >
+        <i className="fas fa-user"></i>
+      </div>
+      {showMenu && (
+        <ul className="profile-dropdown">
+          <li className="profile-dropdown_1">{user.username}</li>
+          <li className="profile-dropdown_2">{user.email}</li>
 
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.logout());
-    };
-
-    return (
-        <>
-            <div 
-                id="link-profile" 
-                onClick={openMenu}
-            >
-                <i className="fas fa-user"></i>                
-            </div>
-                {showMenu && (
-                    <ul className="profile-dropdown">
-                        <li className="profile-dropdown_1">{user.username}</li>
-                        <li className="profile-dropdown_2">{user.email}</li>
-                        
-                        <a 
-                            id="link-logout"
-                            onClick={logout}
-                        >
-                            Log Out
-                        </a>
-                    </ul>
-            )}
-        </>
-    );
+          <button
+            id="link-logout"
+            className="link"
+            onClick={logout}
+          >
+            Log Out
+                        </button>
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default ProfileButton;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -7,77 +7,100 @@ import LoginForm from '../LoginForm';
 import './Navigation.css';
 import SignupForm from '../SignupForm';
 
-const Navigation = ({ isLoaded }) =>{
-    const sessionUser = useSelector(state => state.session.user);
-    const [form, setForm] = useState("");
-    const [showModal, setShowModal] = useState(false);
+const Navigation = ({ isLoaded }) => {
+  const sessionUser = useSelector(state => state.session.user);
+  const [form, setForm] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-    const loginButton = (e) => {
-        e.preventDefault();
-        setForm("login");
-        setShowModal("true");
-    }
+  const loginButton = (e) => {
+    e.preventDefault();
+    setForm("login");
+    setShowModal("true");
+  }
 
-    const signupButton = (e) => {
-        e.preventDefault();
-        setForm("signup");
-        setShowModal("true");
-    }
+  const signupButton = (e) => {
+    e.preventDefault();
+    setForm("signup");
+    setShowModal("true");
+  }
 
-    let sessionLinks;
-    if (sessionUser) {
-        sessionLinks = (
-            <>
-                <NavLink 
-                    id="link-bookings"  
-                    to="/bookings"
-                >
-                    My Bookings
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <>
+        <NavLink
+          class="link"
+          id="link-bookings"
+          to="/bookings"
+        >
+          My Bookings
                 </NavLink>
-                <ProfileButton user={sessionUser} />
-            </>
-        );
-    }else {
-        sessionLinks = (
-            <>
-                <a href="/login" id="link-login" onClick={loginButton}>Login</a>
-                <a href="/signup" id="link-signup" onClick={signupButton}>Sign Up</a>
-                {showModal && (
-                    <Modal onClose={() => setShowModal(false)}>
-                        { form === "login" && (
-                            <LoginForm />
-                        )}
-                        { form === "signup" && (
-                            <SignupForm />
-                        )}
-                    </Modal>
-                )}
-            </>
-        );
-    }
-
-    return (
-        <>
-            <ul className='navbar'>
-                <li>
-                    <h2 id="navbar-link-home">
-                        <Link to="/listings">
-                            bike around
-                        </Link>
-                    </h2>
-                </li>
-                <li className="navbar-link">
-                    <NavLink 
-                        to="/" exact
-                        id="link-home"
-                        >Home
-                    </NavLink> 
-                    {isLoaded && sessionLinks}
-                </li>
-            </ul>
-            <hr id="listing-bar" color="#896a67" />
-        </>
+        <NavLink
+          class="link-icon link"
+          id="link-icon-bookings"
+          to="/bookings"
+        >
+        <i className="fas fa-calendar-alt"></i>
+                </NavLink>
+        <ProfileButton user={sessionUser} />
+      </>
     );
+  } else {
+    sessionLinks = (
+      <>
+        <a
+          href="/login"
+          class="link"
+          id="link-login"
+          onClick={loginButton}>
+          Login
+                </a>
+        <a
+          href="/signup"
+          class="link"
+          id="link-signup"
+          onClick={signupButton}>
+          Sign Up
+                </a>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            { form === "login" && (
+              <LoginForm />
+            )}
+            { form === "signup" && (
+              <SignupForm />
+            )}
+          </Modal>
+        )}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <ul className='navbar'>
+        <li>
+          <h2 id="navbar-link-home">
+            <Link 
+              to="/listings"
+              id="link-logo">
+              bike around
+                        </Link>
+          </h2>
+        </li>
+        <li className="navbar-link">
+          <NavLink
+            to="/" exact
+            class="link" 
+            id="link-home"
+          >Home
+                    </NavLink>
+          {isLoaded && sessionLinks}
+        </li>
+      </ul>
+      <hr id="listing-bar" color="#896a67" />
+    </>
+  );
 }
 
 export default Navigation;
